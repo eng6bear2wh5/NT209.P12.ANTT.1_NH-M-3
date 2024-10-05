9ecb3cedf95d393ac6f3456ef4a3e11de840eac5
dwk-hng <nguyenhung20202023@gmail.com>
2024-10-05 12:29:44 +0700

    c21.s

--- Duc_Hung/c21.s
+++ Duc_Hung/c21.s
@@ -1,6 +1,7 @@
 .section .data
      rs:  .string "Love UIT" # khai báo vùng nhớ tên là rs lưu chuỗi "Love UIT"
      rs_len = . -rs          # khai báo hằng số tên là rs_len lưu giá trị độ dài của chuỗi rs (tính cả NULL)
+     newline: .string "\n"   # khai báo vùng nhớ để lưu ký tự xuống dòng
 
 .section .bss
      .lcomm output, 2        # khai báo vùng nhớ tên là output gồm 2 bytes
@@ -16,12 +17,19 @@
      # Ngoài ra ta có thể gộp 2 dòng trên thành một lệnh như sau: 
      # addl $47,(output)     # cộng dữ liệu trong output (độ dài của chuỗi rs tính cả NULL) với 47 để chuyển từ số sang ký tự số của số đó được trừ đi 1 (bỏ NULL)
  
+# xuất ra màn hình độ dài của chuỗi (số kỳ tự - không tính ký tự NULL)
 movl $4, %eax                # sys_write
 movl $1, %ebx                # std_out
 movl $output, %ecx           # địa chỉ ô nhớ output
 movl $2, %edx                # độ dài sẽ xuất ra (bytes)
 int $0x80                    # call kernel
 
-movl $1, %eax                # system call number (sys_exit)
+# xuống dòng
+movl $4, %eax                # sys_write
+movl $1, %ebx                # std_out
+movl $newline, %ecx          # địa chỉ ô nhớ output
+movl $1, %edx                # độ dài sẽ xuất ra (bytes)
 int $0x80                    # call kernel
 
+movl $1, %eax                # system call number (sys_exit)
+int $0x80                    # call kernel
